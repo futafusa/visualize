90dae6250cd22aff3a24bb45722c39614611cc6e
tatsuhiko.sakai <>
2024-12-09 20:03:22 +0900

    add - 引き付けサンプル追加

--- app/routes.ts
+++ app/routes.ts
@@ -15,6 +15,7 @@ export default [
     route("shaderWater", "./routes/visual/shaderWater.tsx"),
     route("sampleVrm", "./routes/visual/sampleVrm.tsx"),
     route("manyCircle", "./routes/visual/manyCircle.tsx"),
+    route("ballAttract", "./routes/visual/ballAttract.tsx"),
     // route("sampleCustomShader", "./routes/visual/sampleCustomShader.tsx"),
     // route("wobble", "./routes/visual/wobble.tsx"),
   ]),
