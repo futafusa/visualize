4510f25f5e8e98607d1e6b4217669d32eefc9611
tatsuhiko.sakai <>
2024-12-11 20:33:41 +0900

    update - shader遊び

--- app/routes/visual/sampleShaderPattern.tsx
+++ app/routes/visual/sampleShaderPattern.tsx
@@ -10,7 +10,7 @@ function ShaderPattern() {
   const { testParam } = useControls('Test', {
     testParam: {
       value: 0.0,
-      min: 0.0,
+      min: -1.0,
       max: 1.0,
       step: 0.01,
     }
@@ -18,19 +18,20 @@ function ShaderPattern() {
 
   const pattern01 = new THREE.ShaderMaterial({
     uniforms: {
+      uTime: { value: 0.0 },
       testParam: { value: testParam },
-    },  
+    },
     vertexShader: pattern01VertexShader,
     fragmentShader: pattern01FragmentShader
   });
 
   useFrame((_, delta) => {
-    // pattern01.uniforms.uTime.value += delta;
+    pattern01.uniforms.uTime.value += delta;
   })  
 
   return (
     <mesh position={[0, 0.01, 0]} rotation-x={-Math.PI * 0.5} material={pattern01}>
-      <planeGeometry args={[2, 2, 512, 512]}/>
+      <planeGeometry args={[1.6, 0.9, 512, 512]}/>
     </mesh>
   )
 }
--- app/shaders/pattern01/fragment.glsl
+++ app/shaders/pattern01/fragment.glsl
@@ -1,6 +1,9 @@
 varying vec2 vUv;
+uniform float uTime;
 uniform float testParam;
 #define PI 3.1415926535897932384626433832795
+
+#include "../_includes/cnoise2d.glsl"
 
 float random(vec2 st) {
   return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
@@ -10,10 +13,12 @@ void main() {
   // グラデーション
   // float strength = vUv.x * 10.0;
 
+  // float strength = step(testParam, vUv.x);
+
   // 縞模様
   // float strength = mod(vUv.x * 10.0, 1.0);
   // strength = step(0.5, strength);
-  
+
   // 縞模様応用
   // float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
   // strength += step(0.8, mod(vUv.y * 10.0, 1.0));
@@ -30,7 +35,7 @@ void main() {
   // float strength = abs(vUv.x - 0.5);
 
   // float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
-  
+
   // 反転して、すこし小さくして乗算
   // float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
   // strength *= 1.0 - step(0.25, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
@@ -63,13 +68,46 @@ void main() {
   // float strength = 1.0 - step(0.02, abs(distance(waveUv, vec2(0.5)) - 0.25));
 
   // atanを使った角度の応用
-  float angle = atan(vUv.x - 0.5, vUv.y - 0.5) / (PI * 2.0) + 0.5;
+  // float angle = atan(vUv.x - 0.5, vUv.y - 0.5) / (PI * 2.0) + 0.5;
   // float strength = angle;
+
   // float strength = mod(angle * 3.0, 1.0);
   // float strength = sin(angle * 50.0);
-  // atanを使った角度の応用2
-  float radius = 0.25 + sin(angle * 100.0) * 0.02;
-  float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - radius));
 
-  gl_FragColor = vec4(strength, strength, strength, 1.0);
+  // atanを使った角度の応用2
+  // float radius = 0.25 + sin(angle * 100.0) * 0.02;
+  // float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - radius));
+
+  // ノイズ関数を使ったサンプル
+  // float strength = cnoise(vUv * 20.0);
+  // float strength = step(testParam, cnoise(vUv * 10.0));
+  // float strength = 1.0 - abs(cnoise(vUv * 10.0));
+  // float strength = step(testParam, sin(cnoise(vUv * 10.0) * 20.0 ));
+  // vec3 color = vec3(strength);
+
+  // vec3 backColor = vec3(0.0);
+  // vec3 frontColor = vec3(vUv, 1.0);
+  // vec3 color = mix(backColor, frontColor, strength);
+
+  // オリジナル
+  // float noize1 = step(0.5, sin(cnoise(vUv * 10.0) * 20.0 ));
+
+  // float noize = step(testParam, sin(cnoise(vUv * 10.0) * 20.0 ));
+
+  // float dist = distance(vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5), vec2(0.5, 2.0 * testParam));
+  // float fade = 1.0 - smoothstep(0.0, 0.8, dist);
+  float noise = cnoise(vec2(vUv.x * 20.0, vUv.y * 20.0)) * 0.3;
+  // 中心で1.0、端で0.0
+  float distanceFromCenter = 1.0 - abs(vUv.x - 0.5) * 2.0;
+  noise *= distanceFromCenter;
+
+  float gradient = step(testParam, vUv.y +noise);
+  // float gradient = step(testParam + noise * smoothstep(0.0, 0.8, testParam) * smoothstep(1.0, 0.2, testParam), vUv.y);
+  // vec3 strength = vec3(gradient);
+
+  // vec3 backColor = vec3(0.0);
+  // vec3 frontColor = vec3(vUv, 1.0);
+  // vec3 color = mix(backColor, frontColor, strength);
+
+  gl_FragColor = vec4(vec3(gradient), 1.0);
 }
